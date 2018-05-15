a520e702c31428d3915c45788c6a81223777acd2
Julio Nicolás Santarelli <santarellijulio@gmail.com>
2018-05-15 17:09:34 -0300

    GPC Visualizacion resultados

--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -10,13 +10,15 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-		<ul class="hpn-container-listado vertical"><?php if ( 'post' === get_post_type() ) : ?>
+
+		<ul class="hpn-container-listado vertical">
 			<li class="hpn-list-item">
 				<img class="w-md h-md circular cover" scr="<?php the_post_thumbnail(); ?>
 					<div class="hpn-bloque-texto">
-						<div class="hpn-container-badges">
+						<div class="hpn-container-badges"><?php if ( 'post' === get_post_type() ) : ?>
 							<div class="hpn-badge verde"><?php hpn_posted_on(); ?></div>
-							<div class="hpn-badge verde"><?php hpn_posted_by(); ?></div>  
+							<div class="hpn-badge verde"><?php hpn_posted_by(); ?></div>
+							<?php endif; ?>
 							<div class="hpn-badge verde"><?php the_category(); ?></div>
 						</div>
 						<div class="hpn-titulo azul-oscuro"><?php the_title( sprintf( '<h2><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?></div>
@@ -25,6 +27,6 @@
 					</div>
 					<button class="hpn-btn rounded txt-xxs w-xs cyan">Ver<?php get_permalink(); ?></button>
 			</li>
-			<?php endif; ?>
 		</ul>
 </article><!-- #post-<?php the_ID(); ?> -->
+
